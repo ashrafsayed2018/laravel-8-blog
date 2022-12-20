0a5d9588aa2a27e1c9d46191bd832554e36816e0
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-20 20:32:06 +0300

    linking the main.js and drop-down.js in head.blade.php in partials

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -15,5 +15,7 @@
 @livewireStyles
 
 <!-- Scripts -->
+<script src="{{ asset("js/main.js") }}" defer></script>
+<script src="{{ asset("js/drop-down.js") }}" defer></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
 <title>@yield('title', 'Blog')</title>
