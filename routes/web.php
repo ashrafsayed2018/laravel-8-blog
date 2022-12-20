7546443723f9294e7d1693f38d071870dc403f0a
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-20 23:11:25 +0300

    create category routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,12 +20,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, "index"])->name("home");
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// dashboard route  with auth middleware and prefix
+Route::group(["middleware" => "auth", "prefix" => "dashboard"], function () {
+    // dashboard route
+    Route::group(['prefix' => '', 'as' => 'dashboard.'], function () {
+        Route::get('/', [DashboardController::class, "index"])->name('index');
+    });
+
+    // categories route
+    Route::group(["prefix" => "categories", "as" => "categories."], function () {
+        Route::get("/", [CategoryController::class, "index"])->name("index");
+        Route::get("create", [CategoryController::class, "create"])->name("create");
+        Route::post("/", [CategoryController::class, "store"])->name("store");
+        Route::get("{category:slug}/edit", [CategoryController::class, "edit"])->name("edit");
+        Route::put("{category:slug}/update", [CategoryController::class, "update"])->name("update");
+        Route::delete("{category:slug}/delete", [CategoryController::class, "delete"])->name("delete");
+    });
 });
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
