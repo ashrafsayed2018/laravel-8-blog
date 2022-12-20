f66f952ef04e05b82b7b2b87880abc42b0f6d9a3
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-20 20:56:12 +0300

    adding the gradient style in base.blade.php in layouts floder  from landing page downloaded from tailwindcss toolbox

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,6 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
       <x-partials.head />
+          <!-- Define your gradient here - use online tools to find a gradient matching your branding-->
+    <style>
+        .gradient {
+          background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
+        }
+      </style>
     </head>
     <body class="leading-normal tracking-normal text-white gradient" >
         <x-partials.nav />
