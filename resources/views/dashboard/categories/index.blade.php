4bb144e68a8b38c7a6fe1f45eede5e371941547f
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-21 02:35:03 +0300

    create categories index table

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -6,21 +6,95 @@
     </x-slot>
 
     <x-slot name="nav">
-       <div class="space-x-4">
-            <x-jet-nav-link href="{{ route('categories.index') }}" :active="request()->routeIs('categories.index')">
-                {{ __('التاجات') }}
-            </x-jet-nav-link>
+    <div class="space-x-4">
+            {{-- <x-jet-nav-link href="{{ route('categories.index') }}" :active="request()->routeIs('categories.index')">
+                {{ __('الاقسام') }}
+            </x-jet-nav-link> --}}
 
             <x-jet-nav-link href="{{ route('categories.create') }}" :active="request()->routeIs('categories.create')">
-                {{ __('التاجات') }}
+
+                <button type="button" class="bg-green-500 text-white px-3 py-2 rounded-md shadow-md shadow-green-500">
+                    {{ __('اضافة قسم') }}
+
+                </button>
             </x-jet-nav-link>
-       </div>
+    </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+              {{-- table for index the categories --}}
+              <table class="w-full divide-y divide-gray-200">
+                <thead class="font-bold text-gray-500 bg-indigo-200">
+                    <tr>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            مسلسل
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            الاسم
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            القسم الفرعي
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            تاريخ الانشاء
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            تاريخ التحديث
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            الاعدادات
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="text-xs divide-y divide-gray-200 bg-indigo-50">
+                    <tr>
+                        <td></td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            id
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            id
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            id
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            id
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            id
+                        </td>
+                        <td class="px-2 py-4 text-sm text-gray-500 whitespace-nowrap">
+                            <div class="flex justify-start space-x-1">
+                                <a href="#" class="p-1 border-2 border-indigo-200 rounded-md">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                      </svg>
+
+                                </a>
+                                {{-- form --}}
+                                <form action="#" method="POST">
+                                    @csrf
+                                    @method("Delete")
+                                    <button type="submit" class="p-1 border-2 border-red-200 rounded-md">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-red-500">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                          </svg>
+
+                                    </button>
+                                </form>
+                            </div>
+
+
+                        </td>
+                    </tr>
+                </tbody>
+              </table>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,7 +25,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="flex px-4 py-6 mx-auto space-x-8 max-w-7xl sm:px-6 lg:px-8">
+                    <div class="flex px-4 py-4 mx-auto  max-w-7xl justify-between items-center sm:px-6 lg:px-8">
                         {{ $header }}
                         @if (isset($nav))
                             {{ $nav }}
