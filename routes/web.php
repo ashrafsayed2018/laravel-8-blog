483a69f5d6f398e7cf0e6fb1f63f307fee616ded
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-22 20:40:44 +0300

    create tag routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,6 +36,17 @@ Route::group(["middleware" => "auth", "prefix" => "dashboard"], function () {
         Route::get("{category:slug}/edit", [CategoryController::class, "edit"])->name("edit");
         Route::put("{category:slug}/update", [CategoryController::class, "update"])->name("update");
         Route::delete("{category:slug}/delete", [CategoryController::class, "destroy"])->name("delete");
+    });
+
+
+    // tag route
+    Route::group(["prefix" => "categories", "as" => "categories."], function () {
+        Route::get("/", [TagController::class, "index"])->name("index");
+        Route::get("create", [TagController::class, "create"])->name("create");
+        Route::post("/", [TagController::class, "store"])->name("store");
+        Route::get("{tag:slug}/edit", [TagController::class, "edit"])->name("edit");
+        Route::put("{tag:slug}/update", [TagController::class, "update"])->name("update");
+        Route::delete("{tag:slug}/delete", [TagController::class, "destroy"])->name("delete");
     });
 });
 
