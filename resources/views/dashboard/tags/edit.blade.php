2af8f523f9da5733224a62f0df05616e60037479
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-22 20:44:27 +0300

    create tag views

---
+++ resources/views/dashboard/tags/edit.blade.php
@@ -0,0 +1,62 @@
+
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('الاقسام') }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="nav">
+        <div class="space-x-4">
+                <x-jet-nav-link href="{{ route('categories.index') }}" :active="request()->routeIs('categories.index')">
+                    {{ __('الاقسام') }}
+                </x-jet-nav-link>
+
+                <x-jet-nav-link href="{{ route('categories.create') }}" :active="request()->routeIs('categories.create')">
+                    {{ __('اضافة قسم') }}
+                </x-jet-nav-link>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+               <div class="p-4">
+                    <form action="{{ route("categories.update", $category) }}" method="POST">
+                        @csrf
+
+                        @method("put")
+
+                        @if ($category->parent_id != NULL)
+                        <div>
+                            <small class="block font-medium text-sm text-gray-700 mb-4">اختر قسم رئيسي للاقسام الفرعيه فقط </small>
+                            <select name="parent_id" class="w-full mb-6 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+                                <option value="">اختر القسم الرئيسي</option>
+                                @foreach ($categories as $cat)
+
+
+                                <option value="{{ $cat->id }}" {{ $category->parent_id == $cat->id ? 'selected' : '' }} >{{ $cat->name }}</option>
+                                @endforeach
+                            </select>
+                         </div>
+
+                        @endif
+
+
+                        <div>
+                            <x-jet-label for="name" value="{{ __('الاسم') }}" class="mb-4 text-gray-500" />
+                            <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="$category->name"  autofocus autocomplete="name" />
+                            <span class="block text-xs text-red-700 mt-3">اقصي عدد لحروف 50 حرف </span>
+                            <x-jet-input-error for="name" class="mt-2" />
+                        </div>
+
+                        <x-jet-button class="ml-4 mt-6">
+                            {{ __('تحديث') }}
+                        </x-jet-button>
+                    </form>
+               </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/dashboard/tags/index.blade.php
@@ -0,0 +1,131 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('الاقسام') }}
+        </h2>
+    </x-slot>
+
+    <x-slot name="nav">
+        <div class="space-x-4">
+                <x-jet-nav-link href="{{ route('categories.index') }}" :active="request()->routeIs('categories.index')">
+                    {{ __('الاقسام') }}
+                </x-jet-nav-link>
+
+                <x-jet-nav-link href="{{ route('categories.create') }}" :active="request()->routeIs('categories.create')">
+                    {{ __('اضافة قسم') }}
+                </x-jet-nav-link>
+        </div>
+    </x-slot>
+
+    {{-- alert --}}
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
+        <x-ui.alerts />
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-auto shadow-xl sm:rounded-lg">
+              {{-- table for index the categories --}}
+              @if ($categories->count() > 0)
+              <table class="w-full divide-y divide-gray-200">
+                <thead class="font-bold text-gray-500 bg-indigo-200">
+                    <tr>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            مسلسل
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            الاسم
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            النوع
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            القسم الفرعي
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            تاريخ الانشاء
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            تاريخ التحديث
+                        </th>
+                        <th class="px-2 py-3 text-xs tracking-wide text-right">
+                            الاعدادات
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="text-xs divide-y divide-gray-200 bg-indigo-50">
+
+                    <?php $i = 1; ?>
+                    @foreach ($categories as $category)
+                    <tr>
+                        <td></td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            {{ $i++}}
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            {{ $category->name }}
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            @if ($category->parent_id == NULL)
+                              <span class="text-green-500 font-semibold">قسم رئيسي</span>
+                            @else
+                              <span class="text-red-500 font-semibold">قسم فرعي</span>
+                            @endif
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            <ul class="flex">
+                                [
+                                @foreach ($category->subCategories as $subCategory )
+
+                                 <li>
+                                    {{ $subCategory->name }}
+                                 </li>
+                                ,
+                                @endforeach
+
+                                ]
+                            </ul>
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            {{ $category->created_at->format("d/m/y") }}
+                        </td>
+                        <td class="px-2 py-4 whitespace-nowrap">
+                            {{ $category->updated_at->format("d/m/y") }}
+                        </td>
+                        <td class="px-2 py-4 text-sm text-gray-500 whitespace-nowrap">
+                            <div class="flex justify-start space-x-1">
+                                <a href="{{ route("categories.edit", $category) }}" class="p-1 border-2 border-indigo-200 rounded-md">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                      </svg>
+
+                                </a>
+                                {{-- form --}}
+                                <form action="{{ route("categories.delete",$category) }}" method="POST">
+                                    @csrf
+                                    @method("Delete")
+                                    <button type="submit" class="p-1 border-2 border-red-200 rounded-md">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-red-500">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                          </svg>
+
+                                    </button>
+                                </form>
+                            </div>
+
+
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+              </table>
+              @else
+                 <div class="p-2 text-white text-center font-bold bg-red-500 rounded">لا توجد اقسام </div>
+              @endif
+            </div>
+        </div>
+    </div>
+</x-app-layout>
