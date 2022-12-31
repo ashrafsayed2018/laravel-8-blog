2ae75749ce0eec844a359a46f4ccd12a9c328b4a
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-31 23:06:35 +0300

    include choices.css in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="{{ asset("css/choices.css") }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -15,6 +16,8 @@
 
         <!-- Scripts -->
         {{-- <script defer src="https://unpkg.com/alpinejs@3.2.4/dist/cdn.min.js"></script> --}}
+
+
 
         <script src="{{ asset('js/app.js') }}" defer></script>
 
