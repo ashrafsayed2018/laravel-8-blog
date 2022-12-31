80f2009776c27a80b092ee8508b6010cd7d44b42
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-31 23:13:48 +0300

    import and install custom.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,13 @@ require('alpinejs');
 
 
 Alpine.start();
+import Choices from "choices.js";
+
+// create multi-select element
+
+window.choices = (element) => {
+    return new Choices(element, {
+          maxItemCount: 4,
+          removeItemButton: true,
+    });
+}
