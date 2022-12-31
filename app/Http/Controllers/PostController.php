98d5e7d5fe84b737595a650a2975304bb3d3cba6
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-12-31 16:03:06 +0300

    save post image

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StorePostRequest;
 use App\Models\Tag;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use App\Http\Requests\StorePostRequest;
 
 class PostController extends Controller
 {
@@ -39,11 +40,42 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        $post = new Post();
-        $post->title = $request->title;
-        $post->body = $request->body;
+
+
+
+        $post                   = new Post();
+        $post->title            = $request->title;
+        $post->slug             = make_slug($request->title);
+        $post->body             = $request->body;
+        $post->category_id      = $request->category_id;
+        $tags                   = explode(',', $request->tags);
+        $post->published_at     = $request->published_at;
+        $post->meta_description = $request->meta_description;
+
+        // chech if the request has a file
+        if ($request->hasFile("cover_image")) {
+
+            $image              = $request->file("cover_image");
+            $imageName          = $image->getClientOriginalName();
+            $imageNewName = pathinfo($imageName, PATHINFO_FILENAME);
+            $fileExtension      = time() . "." . $imageNewName . "." . $image->getClientOriginalExtension();
+
+            // image location
+            $location           = storage_path("images/" . $fileExtension);
+            // resize the image and save it in file path
+            Image::make($image)->resize(1200, 630)->save($location);
+
+            // save the image in db
+            $post->cover_image      = $fileExtension;
+        }
+        // save the post
+
+        $post->save();
+        $post->sync($tags);
+
+        return redirect()->route("post.index")->with("success", "تم اضافة المقال بنجاح");
     }
 
     /**
