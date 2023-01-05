6bc73661738160ce288f1d3474f3fd526ea2183a
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-01-05 14:17:35 +0300

    create method in livewire class

--- app/Http/Livewire/Post/ShowPost.php
+++ app/Http/Livewire/Post/ShowPost.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Livewire\Post;
 
+use App\Models\Post;
 use Livewire\Component;
+use App\Models\Category;
+use Livewire\WithPagination;
 
 class ShowPost extends Component
 {
+
+    use WithPagination;
+    public $category;
+    public $sortBy = 'recentdesc';
+    protected $queryString = [
+        'category' => ['except' => ''],
+        'sortBy'   => ['except' => 'recentdesc']
+    ];
     public function render()
     {
+
+        $categories = Category::all();
+        $posts      = Post::all();
         return view('livewire.posts.show-post');
     }
 }
