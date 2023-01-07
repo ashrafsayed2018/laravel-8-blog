f3c6db5a0aa90d9cb2b97b266cb93aec815d83fe
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-01-07 14:01:58 +0300

    edit livewire newsletter form

--- app/Http/Livewire/NewsLetter/Form.php
+++ app/Http/Livewire/NewsLetter/Form.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Form extends Component
 {
+
+    public string $name  = '';
+    public string $email = '';
+
+    public function formSubmit()
+    {
+        dd("hello");
+    }
     public function render()
     {
         return view('livewire.news-letter.form');
--- resources/views/livewire/news-letter/form.blade.php
+++ resources/views/livewire/news-letter/form.blade.php
@@ -1,3 +1,26 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="mb-4">
+    <x-ui.alerts />
+    <form wire:submit.prevent="formSubmit">
+      <div class="container max-w-xl mx-auto space-y-4 text-right">
+              {{-- name --}}
+              <div >
+                <x-jet-label for="name" value="{{ __('الاسم') }}" class="mb-4 font-bold text-white" />
+                <x-jet-input name="name" wire:model="name" id="name" class="block mt-1 w-full" type="text" />
+                <x-jet-input-error for="name" class="mt-2" />
+            </div>
+            {{-- email --}}
+            <div >
+                <x-jet-label for="email" value="{{ __('الايميل') }}" class="mb-4 font-bold text-white" />
+                <x-jet-input email="email" wire:model="email" id="email" class="block mt-1 w-full" type="text" />
+                <x-jet-input-error for="email" class="mt-2" />
+
+                {{ $email }}
+            </div>
+         {{-- submit button --}}
+         <div>
+            <x-jet-button class="mt-4">اشتراك </x-jet-button>
+         </div>
+      </div>
+    </form>
 </div>
+
