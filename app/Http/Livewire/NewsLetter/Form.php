777dce370479e332dfd7eefbaab60db02016b53c
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-01-07 13:41:04 +0300

    create newsletter form livewire component

---
+++ app/Http/Livewire/NewsLetter/Form.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\NewsLetter;
+
+use Livewire\Component;
+
+class Form extends Component
+{
+    public function render()
+    {
+        return view('livewire.news-letter.form');
+    }
+}
